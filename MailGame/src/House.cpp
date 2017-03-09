6cd6c95af58966ac9346f5968af1bc59cb3b3154
josefwaller <josef@siriusapplications.com>
2017-03-09 09:02:12 -0800

    Added House skeleton code.

---
+++ MailGame/src/House.cpp
@@ -0,0 +1,15 @@
+#include "House.h"
+
+House::House(Vector2f pos) : Building(pos)
+{
+
+}
+void House::update(Time dt)
+{
+
+}
+
+void House::render(RenderWindow * window)
+{
+
+}
---
+++ MailGame/src/House.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+
+#include "Building.h"
+
+using namespace std;
+using namespace sf;
+
+class House : public Building
+{
+public:
+	House(Vector2f pos);
+	void update(Time dt);
+	void render(RenderWindow * window);
+
+private:
+	// variables used for timing letter generation
+	long lastMailTime;
+	static const long mailInterval = 200;
+};
