c2d3e456495139e2162af61a01fcff5a64eacd3c
josefwaller <josef@siriusapplications.com>
2017-03-16 10:03:39 -0700

    Added UiElement, a base class for all UI elements.

---
+++ MailGame/src/UiElement.cpp
@@ -0,0 +1,10 @@
+#include "UiElement.h"
+
+UiElement::UiElement(int x, int y, int w, int h)
+{
+	// sets position
+	this->x = x;
+	this->y = y;
+	this->w = w;
+	this->h = h;
+}
---
+++ MailGame/src/UiElement.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+
+using namespace std;
+using namespace sf;
+
+// Base class for all UiElements
+class UiElement
+{
+public:
+	UiElement(int x, int y, int w, int h);
+
+	virtual void onEvent(Event e) = 0;
+
+	virtual void render(RenderWindow * window) = 0;
+
+
+private:
+	int x;
+	int y;
+	int w;
+	int h;
+
+};
